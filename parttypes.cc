6227140ecbf197b706e5f3f48c2d190788960fb1
nl6720 <nl6720@gmail.com>
2020-08-22 16:20:14 +0300

    Add systemd-homed user's home partition type
    See https://systemd.io/HOME_DIRECTORY/ .

--- parttypes.cc
+++ parttypes.cc
@@ -148,6 +148,8 @@ void PartType::AddAllTypes(void) {
    AddType(0x830F, "86ED10D5-B607-45BB-8957-D350F23D0571", "Linux IA-64 root verity");
    AddType(0x8310, "4D21B016-B534-45C2-A9FB-5C16E091FD2D", "Linux /var"); // Linux /var (auto-mounted by systemd)
    AddType(0x8311, "7EC6F557-3BC5-4ACA-B293-16EF5DF639D1", "Linux /var/tmp"); // Linux /var/tmp (auto-mounted by systemd)
+   // https://systemd.io/HOME_DIRECTORY/
+   AddType(0x8312, "773F91EF-66D4-49B5-BD83-D683BF40AD16", "Linux user's home"); // used by systemd-homed
 
    // Used by Intel Rapid Start technology
    AddType(0x8400, "D3BFE2DE-3DAF-11DF-BA40-E3A556D89593", "Intel Rapid Start");
